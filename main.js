c39a944f91ba3b29481986c7e5c5915112269bd5
1-mauricio <mauriiciioallves@hotmail.com>
2021-08-14 00:27:03 -0300

    aula 4

--- main.js
+++ main.js
@@ -18,16 +18,20 @@ for (const link of links) {
 }
 
 /*mudar o header da página quando der scroll*/
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
+
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header');
+    const navHeight = header.offsetHeight;
+    
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
+
+
 
 /*Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -46,11 +50,31 @@ const scrollReveal = ScrollReveal({
     distance:'30px',
     duration: 700,
     reset:true
-})
+});
 
 scrollReveal.reveal(
     `#home .text, #home .image,
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links`, {interval: 100})
+    #contact .text, #contact .links,
+    footer .brand, footer .social`, 
+    {interval: 100});
+
+
+/* Botão voltar para o topo*/
+function backToTopButton() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if(window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/*When Scroll**/
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTopButton()
+})
