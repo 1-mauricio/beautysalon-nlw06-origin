f4af71883c47d759f5f8c215f8f0181def42c0a8
1-mauricio <mauriiciioallves@hotmail.com>
2021-08-15 01:49:38 -0300

    aula 5

--- main.js
+++ main.js
@@ -20,10 +20,9 @@ for (const link of links) {
 /*mudar o header da página quando der scroll*/
 
 
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 function changeHeaderWhenScroll() {
-    const header = document.querySelector('#header');
-    const navHeight = header.offsetHeight;
-    
     if(window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
@@ -41,6 +40,12 @@ const swiper = new Swiper('.swiper-container', {
     },
     mousewheel: true,
     keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
   });
 
 
@@ -63,9 +68,8 @@ scrollReveal.reveal(
 
 
 /* Botão voltar para o topo*/
-function backToTopButton() {
-    const backToTopButton = document.querySelector('.back-to-top')
-
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
     if(window.scrollY >= 560) {
         backToTopButton.classList.add('show')
     } else {
@@ -73,8 +77,34 @@ function backToTopButton() {
     }
 }
 
+/*Menu ativo conforme a seção visivel na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+ 
+
 /*When Scroll**/
 window.addEventListener('scroll', function() {
     changeHeaderWhenScroll()
-    backToTopButton()
+    backToTop()
 })
